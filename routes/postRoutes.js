67a1bae015d91918adea54ad952e40b0d2928017
tjr226 <tjr226@gmail.com>
2019-07-09 12:33:42 -0400

    added GET comments by post ID

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -25,19 +25,37 @@ router.get('/:id', function (req, res) {
     const { id } = req.params;
 
     db.findById(id)
-    .then(post => {
-        if (post.length === 0) {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        } else {
-            res.status(200).json(post);
-        }
-    })
-    .catch(error => {
-        res.status(500).json({ error: "The post information could not be retrieved." });
-    })
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
 })
 
 // GET comments
+
+router.get('/:id/comments', function (req, res) {
+    const { id } = req.params;
+
+    db.findPostComments(id)
+        .then(comments => {
+            // note - the DB returns [] in both cases
+            // no comments, or no original post
+            if (comments.length === 0) {
+                res.status(404).json({ message: "The post either does not exist or has zero comments." });
+            } else {
+                res.status(200).json(comments);
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The comments information could not be retrieved." })
+        })
+})
 
 // DELETE blog post
 
