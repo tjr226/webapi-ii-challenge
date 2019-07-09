61a85ad9d6bbe1f84183014f50aaf451867c97b8
tjr226 <tjr226@gmail.com>
2019-07-09 12:24:45 -0400

    added GET, GET by ID routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,8 +3,45 @@ const router = express.Router();
 
 const db = require('../data/db.js');
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: "routes working" });
+// POST blog post
+
+// POST comment
+
+// GET all blog posts
+
+router.get('/', function (req, res) {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
 })
+
+// GET blog post by ID
+
+router.get('/:id', function (req, res) {
+    const { id } = req.params;
+
+    db.findById(id)
+    .then(post => {
+        if (post.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json(post);
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    })
+})
+
+// GET comments
+
+// DELETE blog post
+
+// PUT blog post
+
 
 module.exports = router;
